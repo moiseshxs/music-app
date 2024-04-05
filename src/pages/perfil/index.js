880bbe96bec493ba927f8a781dae3237f34f1242
moiseshxs <moiseshenryb@gmail.com>
2024-04-05 15:13:03 -0300

    Modal de atualizar perfil

--- src/pages/perfil/index.js
+++ src/pages/perfil/index.js
@@ -1,16 +1,25 @@
-import React, { useState, useEffect } from 'react';
-import { Text, View, SafeAreaView, TextInput, ScrollView, StatusBar } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
-import styles from './styles';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import React, { useState, useEffect } from "react";
+import {
+  Text,
+  View,
+  SafeAreaView,
+  Modal,
+  TouchableOpacity,
+  TextInput,
+  StatusBar,
+} from "react-native";
+import styles from "./styles";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export default function Perfil() {
   const [user, setUser] = useState(null);
+  const [username, setUsername] = useState('');
+  const [isModalVisible, setIsModalVisible] = useState(false);
 
   useEffect(() => {
     const getUserData = async () => {
       try {
-        const userData = await AsyncStorage.getItem('user');
+        const userData = await AsyncStorage.getItem("user");
         if (userData !== null) {
           setUser(JSON.parse(userData));
         }
@@ -22,12 +31,34 @@ export default function Perfil() {
     getUserData();
   }, []);
 
-  {/* <Text style={styles.ParecidoComNome}>{user ? user.username : ''}</Text> */ }
+  const salvar = async () => {
+    const dadosUser = {
+      username: username,
+    };
+
+    const axiosConfig = {
+      headers: {
+        /*         'Accept': 'application/json',
+         */ "Content-Type": "application/x-www-form-urlencoded",
+      },
+    };
+    try {
+      const response = await axios.post(
+        "http://localhost:9000/api/clienteUpdate",
+        dadosUser,
+        axiosConfig
+      );
+      console.log(response.data);
+    } catch (error) {
+      console.error("Erro ao atualizar", error);
+      return false;
+    }
+  };
+
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar barStyle="light-content" />
       <View style={styles.container}>
-
         <View style={styles.areaHeader}>
           <View style={styles.areaCabecalho}>
             <View style={styles.areaFoto}>
@@ -36,27 +67,75 @@ export default function Perfil() {
 
             <View style={styles.areaInfos}>
               <View style={styles.areaNome}>
-                <Text style={styles.textNome}>{user ? user.username : ''}</Text>
+                <Text style={styles.textNome}>{user ? user.username : ""}</Text>
               </View>
 
               <View style={styles.areaSeguidor}>
                 <Text style={styles.textSeguidor}>
-                  <Text style={styles.bold}>6 </Text> seguidores <Text style={styles.bold}>• 22 </Text> seguindo
+                  <Text style={styles.bold}>6 </Text> seguidores{" "}
+                  <Text style={styles.bold}>• 22 </Text> seguindo
                 </Text>
               </View>
             </View>
           </View>
 
           <View style={styles.areaEditar}>
-            <View style={styles.botao}>
-              <Text style={styles.textBotao}>Editar</Text>
-            </View>
+            <TouchableOpacity onPress={() => setIsModalVisible(true)}>
+              <View style={styles.botao}>
+                <Text style={styles.textBotao}>Editar</Text>
+              </View>
+            </TouchableOpacity>
+
+            <Modal
+              visible={isModalVisible}
+              onRequestClose={() => setIsModalVisible(false)}
+              animationType="slide"
+              presentationStyle="overFullScreen"
+              transparent="true"
+            >
+              <View style={styles.areaModal}>
+                <View style={styles.cimaModal}></View>
+                <View style={styles.areaModalHeader}>
+                  <View style={styles.areaModalBotao}>
+                    <TouchableOpacity onPress={() => setIsModalVisible(false)}>
+                      <Text style={styles.textBotaoModal}>Cancelar</Text>
+                    </TouchableOpacity>
+                  </View>
+
+                  <View style={styles.areaModalTitle}>
+                    <Text style={styles.textTitleModal}>Editar perfil</Text>
+                  </View>
+
+                  <View style={styles.areaModalBotao2}>
+                    <TouchableOpacity onPress={salvar()}>
+                      <Text style={styles.textBotaoModal}>Salvar</Text>
+                    </TouchableOpacity>
+                  </View>
+                </View>
+
+                <View style={styles.areaRestoModal}>
+                  <View style={styles.areaFotoModal}>
+                    <View style={styles.fotoModal}></View>
+                  </View>
+
+                  <View style={styles.areaFormModal}>
+                    <View style={styles.areaNomeModal}>
+                      <Text style={styles.textInputModal}>Nome</Text>
+                    </View>
+                    <View style={styles.areaInputModal}>
+                      <TextInput
+                        style={styles.input}
+                        onChangeText={setUsername}
+                        value={username}
+                      />
+                    </View>
+                  </View>
+                </View>
+              </View>
+            </Modal>
           </View>
-
         </View>
-        <View style={styles.areaPlaylist}>
-
-        </View>
+        <View style={styles.areaPlaylist}></View>
       </View>
     </SafeAreaView>
   );
--- src/pages/perfil/styles.js
+++ src/pages/perfil/styles.js
@@ -74,4 +74,81 @@ export default StyleSheet.create({
         flex: 3,
         justifyContent: 'center',
     },
+
+    //MODAL STYLE
+    areaModal: {
+        flex: 1,
+    },
+    cimaModal:{
+        flex: 1,
+    },
+    areaModalHeader: {
+        flex: 1,
+        flexDirection: 'row',
+        borderBottomWidth: 1,
+        borderColor: 'grey',
+        paddingHorizontal: 20,
+        backgroundColor: '#101010',
+        borderTopStartRadius: 20,
+        borderTopEndRadius: 20,
+    },
+    areaModalBotao: {
+        flex: 1,
+        justifyContent: 'center',
+    },
+    areaModalBotao2: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'flex-end',
+    },
+    areaModalTitle: {
+        flex: 2,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    textBotaoModal: {
+        fontSize: 15,
+        color: 'white',
+    },
+    textTitleModal: {
+        fontSize: 15,
+        color: 'white',
+        fontWeight: 'bold',
+    },
+    areaRestoModal: {
+        flex: 10,
+        backgroundColor: '#121212',
+    },
+    areaFotoModal: {
+        flex: 2,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    fotoModal: {
+        height: 150,
+        width: 150,
+        borderRadius: 100,
+        backgroundColor: 'pink',
+    },
+    areaFormModal: {
+        flex: 4,
+        flexDirection: 'row',
+        padding: 20,
+        
+    },
+    areaNomeModal: {
+        flex: 1,
+    },
+    textInputModal: {
+        fontWeight: 'bold',
+        color: 'white',
+        fontSize: 15,
+    },
+    areaInputModal: {
+        flex: 2,
+    },
+    input: {
+       fontSize: 15,
+       color: 'white'
+    },
 });
